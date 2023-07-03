598f0e7cea8f941bdcb37245fb59c388e9b12be7
MihaiDavid05 <44817698+MihaiDavid05@users.noreply.github.com>
2023-07-04 00:51:17 +0300

    Added new notebook version

--- modn/scripts/evaluation_utils.py
+++ modn/scripts/evaluation_utils.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
@@ -16,6 +17,8 @@ def get_plots_path(model_name):
 def plot(stages, results, plots_path, label, ylim):
     """Plot the metrics"""
 
+    matplotlib.rcParams.update({'font.size': 17})
+
     if label == 'RMSE':
         point_label = 'min'
         best_idx_score = stages[np.nanargmin(results)]
@@ -29,14 +32,16 @@ def plot(stages, results, plots_path, label, ylim):
     ax.plot(stages, results)
     ax.plot(best_idx_score, best_score, 'o',
             label='{}: {:.2f}, timestep: {}'.format(point_label, best_score, best_idx_score),
-            markersize=3, color='orange')
+            markersize=8, color='orange')
     point = mlines.Line2D([], [], color='orange', marker='o', linestyle='None', markersize=5, label=point_label)
     ax.legend(handles=[point])
     ax.set(xlabel=r"Timestep", ylabel=label)
-    ax.set_xlim(-2, len(stages) + 1)
+    ax.set_xlim(-2, len(stages))
     ax.set_ylim(0, ylim)
     ax.legend()
     plt.tight_layout()
+    # plt.rcParams['figure.figsize'] = (10, 10)
+
     fig.savefig(plots_path)
 
 
@@ -47,18 +52,18 @@ def generate_plots(model, plots_path, feature_decoding, reset_state, loss_f1, st
         for f in model.feature_decoders_cont:
             rmse_res = [results[stage][f"{f}_rmse"]
                         if results[stage].get(f"{f}_rmse") else np.nan for stage in stages[:-1]]
-            plots_name = "{}_rmse{}_best{}.pdf".format(f, '_reset_state' if reset_state else '', loss_f1)
+            plots_name = "{}_rmse{}_best{}.png".format(f, '_reset_state' if reset_state else '', loss_f1)
             path = os.path.join(plots_path, plots_name)
-            plot(stages[:-1], rmse_res, path, 'RMSE', 2)
+            plot(stages[:-1], rmse_res, path, 'RMSE', 1.25)
 
         for f in model.feature_decoders_cat:
             f1_res = [results[stage][f"{f}_f1"] for stage in stages[:-1]]
-            plots_name = "{}_f1{}_best{}.pdf".format(f, '_reset_state' if reset_state else '', loss_f1)
+            plots_name = "{}_f1{}_best{}.png".format(f, '_reset_state' if reset_state else '', loss_f1)
             path = os.path.join(plots_path, plots_name)
             plot(stages[:-1], f1_res, path, 'Macro F1', 1.1)
 
     for f in model.decoders:
         f1_res = [results[stage][f"{f}_f1"] for stage in stages]
-        plots_name = "{}_f1{}_best{}.pdf".format(f, '_reset_state' if reset_state else '', loss_f1)
+        plots_name = "{}_f1{}_best{}.png".format(f, '_reset_state' if reset_state else '', loss_f1)
         path = os.path.join(plots_path, plots_name)
         plot(stages, f1_res, path, 'Macro F1', 1.1)
